c6cd64f5aa03d0d023ba621bb8b06b12d325c8c2
MayankRai30 <102607416+MayankRai30@users.noreply.github.com>
2023-10-18 19:04:26 +0530

    Update cbb.py

--- plugins/cbb.py
+++ plugins/cbb.py
@@ -10,7 +10,7 @@ async def cb_handler(client: Bot, query: CallbackQuery):
     data = query.data
     if data == "about":
         await query.message.edit_text(
-            text = f"<b>○ Creator : <a href='tg://user?id={OWNER_ID}'>This Person</a>\n○ Language : <code>Python3</code>\n○ Library : <a href='https://docs.pyrogram.org/'>Pyrogram asyncio {__version__}</a>\n○ Source Code : <a href='https://github.com/MayankRai30/File'>Click here</a>\n○ Channel : @FLAME_MAINCHATS\n○ Support Group : @FLAME_MAINCHATS</b>",
+            text = f"<b>○ Creator : <a href='tg://user?id={OWNER_ID}'>This Person</a>\n○ Language : <code>Python3</code>\n○ Library : <a href='https://docs.pyrogram.org/'>Pyrogram asyncio {__version__}</a>\n○ Source Code : <a href='https://github.com/MayankRai30/File'>Click here</a>\n○ Channel : @FLAME_NETWORK\n○ Support Group : @FLAME_MAINS</b>",
             disable_web_page_preview = True,
             reply_markup = InlineKeyboardMarkup(
                 [
